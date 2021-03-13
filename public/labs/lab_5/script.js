60f8af8e9d6b831bc09a7b3a316293315afd8f71
ColinMannix <78052098+ColinMannix@users.noreply.github.com>
2021-03-12 22:24:55 -0500

    Revert "Functional without markers"
    This reverts commit ce613f91e1b5c96117203ff9b36f6f2e947dd601.

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,20 +1,8 @@
-function mapInit() {
-  // follow the Leaflet Getting Started tutorial here
-  let map = L.map('mapid').setView([51.505, -0.09], 13);
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiY21hbm5peDEiLCJhIjoiY2ttNndkNTE3MHNkdDJ5bzl5dWltamYwYyJ9.EX7hr3OsYksLFp_m5hWU9Q'
-}).addTo(map);
-  return map;
-}
+/*
+Authors: Jonah Pool, Cole Daitch, Colin Mannix
+*/
 
-async function dataHandler(mapObjectFromFunction) {
-  // use your assignment 1 data handling code here
-  // and target mapObjectFromFunction to attach markers
+async function windowActions() {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
   const request = await fetch(endpoint);
@@ -23,10 +11,11 @@ async function dataHandler(mapObjectFromFunction) {
   function findMatches(wordToMatch, restaurants) {
     const tempArr = restaurants.filter(place => {
       const regex = new RegExp(wordToMatch, 'gi');
-      return place.city.match(regex) || place.name.match(regex) || place.category.match(regex)
+      return place.name.match(regex) || place.name.match(regex) || place.category.match(regex)
     });
     const tempSet = new Set(tempArr.map(place => place.establishment_id));
     const unique_ids = Array.from(tempSet).slice(0,9);
+    console.log(unique_ids);
     let id_length  = unique_ids.length-1;
     const results = [];
     //iterate through the list
@@ -42,39 +31,32 @@ async function dataHandler(mapObjectFromFunction) {
         }
       }
     }
-    
-    return results.slice(0,5);
+    return results;
   }
 
-  function displayMatches(event) {
-    const matchArray = findMatches(event.target.value, restaurants);
-    const html = matchArray.map(place => {
-      return `
-      <div class="box">
-        <li>
-        <address>
-          <h2 class="subtitle is-3"><span class="name">${place.name}</span></h2>
-          <span class="address">${place.address_line_1}</span> <br>
-          ${place.city}, ${place.state} ${place.zip} <br>
-          </br>
-          <span class="category">${place.category} <br>
-          <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</span>
-        </address>
-        </li>
-      </div>
-      `
-    }).join('');
-    suggestions.innerHTML= html;
-  }
-
+function displayMatches(event) {
+  const matchArray = findMatches(event.target.value, restaurants);
+  const html = matchArray.map(place => {
+    return `
+    <div class="box">
+      <li>
+      <address>
+        <h2 class="subtitle is-3"><span class="name">${place.name}</span></h2>
+        <span class="address">${place.address_line_1}</span> <br>
+        ${place.city}, ${place.state} ${place.zip} <br>
+        </br>
+        <span class="category">${place.category} <br>
+        <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</span>
+      </address>
+      </li>
+    </div>
+    `
+  }).join('');
+  suggestions.innerHTML= html;
+}
   const searchInput  = document.querySelector('.input');
   const suggestions = document.querySelector('.suggestions');
 
-  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+  searchInput.addEventListener('submit', (evt) => { displayMatches(evt) });
 }
-
-async function windowActions() {
-  const map = mapInit();
-  await dataHandler(map);
-}
-window.onload = windowActions;
+window.onload = windowActions();
